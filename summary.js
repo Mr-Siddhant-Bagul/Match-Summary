4f2e911f878f9f475756d7200fcc61d97b279ef8
Mr-Siddhant-Bagul <siddhantbagul16@gmail.com>
2023-04-03 09:52:21 +0530

    ?

---
+++ summary.js
@@ -0,0 +1,96 @@
+
+function ViratIn(){
+    document.getElementById("Virat").style.display = "block"
+}
+
+function ViratOut(){
+    document.getElementById("Virat").style.display = "none"
+}
+
+function RDJIn(){
+    document.getElementById("RDJ").style.display = "block"
+}
+
+function RDJOut(){
+    document.getElementById("RDJ").style.display = "none"
+}
+
+function FafIn(){
+    document.getElementById("Faf").style.display = "block"
+}
+
+function FafOut(){
+    document.getElementById("Faf").style.display = "none"
+}
+
+function TusharIn(){
+    document.getElementById("Tushar").style.display = "block"
+}
+
+function TusharOut(){
+    document.getElementById("Tushar").style.display = "none"
+}
+
+function RajatIn(){
+    document.getElementById("Rajat").style.display = "block"
+}
+
+function RajatOut(){
+    document.getElementById("Rajat").style.display = "none"
+}
+
+function DeepakIn(){
+    document.getElementById("Deepak").style.display = "block"
+}
+
+function DeepakOut(){
+    document.getElementById("Deepak").style.display = "none"
+}
+
+function DhoniIn(){
+    document.getElementById("Dhoni").style.display = "block"
+}
+
+function DhoniOut(){
+    document.getElementById("Dhoni").style.display = "none"
+}
+
+function SirajIn(){
+    document.getElementById("Siraj").style.display = "block"
+}
+
+function SirajOut(){
+    document.getElementById("Siraj").style.display = "none"
+}
+
+function RutuIn(){
+    document.getElementById("Rutu").style.display = "block"
+}
+
+function RutuOut(){
+    document.getElementById("Rutu").style.display = "none"
+}
+
+function PatelIn(){
+    document.getElementById("Patel").style.display = "block"
+}
+
+function PatelOut(){
+    document.getElementById("Patel").style.display = "none"
+}
+
+function BenIn(){
+    document.getElementById("Ben").style.display = "block"
+}
+
+function BenOut(){
+    document.getElementById("Ben").style.display = "none"
+}
+
+function JoshIn(){
+    document.getElementById("Josh").style.display = "block"
+}
+
+function JoshOut(){
+    document.getElementById("Josh").style.display = "none"
+}
